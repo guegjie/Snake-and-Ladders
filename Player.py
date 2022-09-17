3c744ce757f28a9a8d27dbe2387d45e6cc00118b
Louis Souttar-Stone <guegjie5@gmail.com>
2022-09-17 18:07:27 +0100

    okay

--- Player.py
+++ Player.py
@@ -1,9 +1,9 @@
-
+import Window
 #ihioh
 
 class Player:
     def __init__(self, name="player", position=0, colour="blue"):
         self.Position = position
 
-    def draw(window):
+    def draw(window=Window):
         window.Canvas.create 
