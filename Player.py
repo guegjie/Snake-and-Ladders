6a385bceab3f1c5316bf39d02d164716901f3bde
Louis Souttar-Stone <guegjie5@gmail.com>
2022-09-17 18:18:45 +0100

    okay

--- Player.py
+++ Player.py
@@ -1,10 +1,9 @@
 import Window
-import tkinter
 #ihioh
 
 class Player:
     def __init__(self, name="player", position=0, colour="blue"):
         self.Position = position
 
-    def draw(window=Window.Window()):
+    def draw(window=Window.Window):
         window.Canvas.create_oval(10, 10, 20, 20)
