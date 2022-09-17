366ce4048db093d0d36734ebb8d27868b7b08a78
Louis Souttar-Stone <guegjie5@gmail.com>
2022-09-17 18:08:32 +0100

    okay

--- Player.py
+++ Player.py
@@ -5,5 +5,5 @@ class Player:
     def __init__(self, name="player", position=0, colour="blue"):
         self.Position = position
 
-    def draw(window=Window):
+    def draw(window=Window.Window()):
         window.Canvas.create 
