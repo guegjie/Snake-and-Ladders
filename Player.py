f15acd95e2b9bfe0aeb9cb22e26416c112ceac8f
Louis Souttar-Stone <guegjie5@gmail.com>
2022-09-19 14:12:50 +0100

    sheeeesh

--- Player.py
+++ Player.py
@@ -7,28 +7,4 @@ class Player:
         self.Colour = colour
 
     def draw(self, window):
-        print(self.binary_search(window.Positions, 0, len(window.Postions), self.Position))
-    
-    def binary_search(self, arr, low, high, x):
- 
-        # Check base case
-        if high >= low:
- 
-            mid = (high + low) // 2
- 
-            # If element is present at the middle itself
-            if arr[mid] == x:
-                return mid
- 
-            # If element is smaller than mid, then it can only
-            # be present in left subarray
-            elif arr[mid] > x:
-                return binary_search(arr, low, mid - 1, x)
- 
-            # Else the element can only be present in right subarray
-            else:
-                return binary_search(arr, mid + 1, high, x)
- 
-        else:
-            # Element is not present in the array
-            return -1
+        window.Canvas.create_oval([window.Positions[self.Position-1][1][0]-window.gridsize[0]/2, window.Positions[self.Position-1][1][1]-window.gridsize[1]/2], [window.Positions[self.Position-1][1][0]+window.gridsize[0]/2, window.Positions[self.Position-1][1][1]+window.gridsize[1]/2], fill="red")
