7b224d1e185da04b5ea6d938c121576a6e61d7b9
Louis Souttar-Stone <guegjie5@gmail.com>
2022-09-19 15:53:58 +0100

    ..

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from Player import Player
 from snakes_ladders import Snake, Ladder
 
 def main():
-    
     MainWindow = Window(Title="Snakes&Ladders")
     playerOne = Player(name="Louis", colour="red")
     playerOne.draw(MainWindow)
