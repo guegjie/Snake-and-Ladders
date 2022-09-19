f0d4cd7322901e89c6e29b12daf8d481a7a3c88d
Louis Souttar-Stone <guegjie5@gmail.com>
2022-09-19 18:20:39 +0100

    main:

--- main.py
+++ main.py
@@ -15,10 +15,16 @@ def main():
     snakes = []
     
     for i in range(0, round(((MainWindow.size[0]/MainWindow.gridsize[0]*MainWindow.size[1]/MainWindow.gridsize[1])/100)*8)):
-        snakes.append(Obstacle(random.randint(16,150-16), random.randint(16,150-16), MainWindow))
+        first = random.randint(16,150-16)
+        second = random.randint(16,first-1)
+        while first-16<=second:
+            first = random.randint(16,150-16)
+            second = random.randint(16,first-1)
+        
+        snakes.append(Obstacle(first, second, MainWindow, "snake"))
     
     for snake in snakes:
-        snake.draw(MainWindow)
+        snake.draw()
     
     MainWindow.mainloop()
 
