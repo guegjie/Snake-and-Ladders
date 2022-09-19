8e757b3c123f695da0d255825abe060e2eee7b10
Louis Souttar-Stone <guegjie5@gmail.com>
2022-09-19 13:09:31 +0100

    rotate

--- snakes_ladders.py
+++ snakes_ladders.py
@@ -1,10 +1,32 @@
+import math
+
 class Snake:
     def __init__(self):
-        pass
+        print(self.Rotate(point=[10, 10], centre=[0,0], rotation=180))
+    
+    def Rotate(self, point=[0,0], centre=[0,0], rotation=0):
+        rotation = math.radians(rotation)
+
+        x = (point[0] * math.cos(rotation)) + (point[1] * math.sin(rotation))
+        y = (point[1] * math.cos(rotation)) - (point[0] * math.sin(rotation))
+
+
+        point_list = [round(x + centre[0]), round(y + centre[1])]
+        return point_list
     
 
 class Ladder:
     def __init__(self):
         pass
+    
+    def Rotate(self, point=[0,0], centre=[0,0], rotation=0):
+        rotation = math.radians(rotation)
 
-#aohopwagophojspdhihfiohwah
+        x = (point[0] * math.cos(rotation)) + (point[1] * math.sin(rotation))
+        y = (point[1] * math.cos(rotation)) - (point[0] * math.sin(rotation))
+
+
+        point_list = [round(x + centre[0]), round(y + centre[1])]
+        return point_list
+    
+snake = Snake()
