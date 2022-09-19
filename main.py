540611c46724fbe5f7a3861d4567be32c46037a1
Louis Souttar-Stone <guegjie5@gmail.com>
2022-09-19 18:11:51 +0100

    main:

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ def main():
     MainWindow = Window(Title="Snakes&Ladders")
     playerOne = Player(name="Louis", colour="red")
     playerOne.draw(MainWindow)
+    
+    
 
     snakes = []
     
-    for i in range(0, 10):
+    for i in range(0, round(((MainWindow.size[0]/MainWindow.gridsize[0]*MainWindow.size[1]/MainWindow.gridsize[1])/100)*8)):
         snakes.append(Obstacle(random.randint(16,150-16), random.randint(16,150-16), MainWindow))
     
     for snake in snakes:
-        snake.draw()
+        snake.draw(MainWindow)
     
     MainWindow.mainloop()
 
