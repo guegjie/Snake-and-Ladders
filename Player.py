a05d76fcfe32a51aadcb87cf1482690e9a683c64
danielbaltruschat <danielbaltruschat@outlook.com>
2022-09-28 18:58:37 +0100

    Co-authored-by: guegjie5@gmail.com <guegjie5@gmail.com> Co-authored-by: yinnfoundhome@gmail.com <yinnfoundhome@gmail.com>

--- Player.py
+++ Player.py
@@ -11,7 +11,6 @@ class Player:
         coords = window.generate_coordinates(self.position)
         radius = self.size[0] / 2
         window.Canvas.create_oval([coords[0]-window.square_size[0]/2, coords[1]-window.square_size[1]/2], [coords[0]+window.square_size[0]/2, coords[1]+window.square_size[1]/2], fill="red")
-<<<<<<< HEAD
         
     def calculateBrightness(self, colour="#FFFFFF", brightness="100"):
         red = colour[1:3]
@@ -19,14 +18,9 @@ class Player:
         blue = colour[5:8]
         colours = [red, green, blue]
         
-        for colour in colours:
-            
-        
-        
-
-player = Player()
-player.calculateBrightness()
-=======
+        for colour in colours:   
+            player = Player()
+            player.calculateBrightness()
     
     def move(self, amount):
         self.position += amount
@@ -37,4 +31,3 @@ player.calculateBrightness()
         self.position = new_location
         if self.position < 1:
             self.position = 1
->>>>>>> 4a44c140c2b3cfbb5582175b6dd6aa6ef3e90253
