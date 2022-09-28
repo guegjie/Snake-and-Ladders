4a44c140c2b3cfbb5582175b6dd6aa6ef3e90253
danielbaltruschat <danielbaltruschat@outlook.com>
2022-09-28 18:36:28 +0100

    added move procedures to player class

--- Player.py
+++ Player.py
@@ -3,11 +3,21 @@ import Window
 
 class Player:
     def __init__(self, name="player", position=1, colour="blue", size=[10,10]):
-        self.Position = position
-        self.Colour = colour
+        self.position = position
+        self.colour = colour
+        self.size = size
 
     def draw(self, window):
-        coords = window.generate_coordinates(self.Position)
-
+        coords = window.generate_coordinates(self.position)
+        radius = self.size[0] / 2
         window.Canvas.create_oval([coords[0]-window.square_size[0]/2, coords[1]-window.square_size[1]/2], [coords[0]+window.square_size[0]/2, coords[1]+window.square_size[1]/2], fill="red")
-        
+    
+    def move(self, amount):
+        self.position += amount
+        if self.position < 1:
+            self.position = 1
+
+    def move_to(self, new_location):
+        self.position = new_location
+        if self.position < 1:
+            self.position = 1
