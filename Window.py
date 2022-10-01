2edae43f5fa9ade0edf485192c0e398de0ab0846
Louis Souttar-Stone <guegjie5@gmail.com>
2022-10-01 20:21:55 +0100

    icon finally

--- Window.py
+++ Window.py
@@ -24,6 +24,7 @@ class Window(tkinter.Tk): #creates a class called window  48/5=10
         self.create_grid() #
         self.add_numbers([2, 2+square_size[1]*round(self.size[1]/square_size[1])])
         self.roll_label = None
+        self.call('wm', 'iconphoto', self._w, tkinter.PhotoImage(file='Icon.png'))
 
     def create_grid(self): # creates grid
         for y in range(0, self.grid_dimensions[0]):
